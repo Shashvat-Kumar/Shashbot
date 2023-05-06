ec12ca456ca2ca292d31cf1437338272e95a8ed0
Shashvat-Kumar <132671738+Shashvat-Kumar@users.noreply.github.com>
2023-05-06 12:30:21 +0530

    This is the AI chatbot.

---
+++ Shashbot.py
@@ -0,0 +1,24 @@
+#Shashbot
+program=True
+while program==True:
+    from newspaper import Article
+    import numpy as np
+    import warnings
+    import nltk
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.metrics.pairwise import cosine_similarity
+    warnings.filterwarnings('ignore')
+    nltk.download('punkt',quiet=True)
+    nltk.download('wordnet',quiet=True)
+    query=input("Hi there! Shashbot here. How can I help you? ")
+    try:
+       from googlesearch import search
+    except ImportError:
+       print("No module named 'google' found")
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+       article= Article(j)
+    article.download()
+    article.parse()
+    article.nlp()
+    corpus=article.text
+    print(corpus)
